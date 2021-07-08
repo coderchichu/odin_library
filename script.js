8ebbdd8925b6cbc05a50c3bc4b7a8492a33be13c
Stephen Huang <prettyaverageshow@gmail.com>
2021-07-08 21:00:04 +1200

    working on the other js book

---
+++ script.js
@@ -0,0 +1,16 @@
+// 210706 Author: Stephen H, Javascript for Library project for OdinProject
+
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    //the constructor...
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+function addBookToLibrary() {
+
+
+}
