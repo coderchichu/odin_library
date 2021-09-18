7ecefb53c582feacc97e41fa97caeef5da8db9b8
Stephen Huang <prettyaverageshow@gmail.com>
2021-09-19 00:51:47 +1200

    yay reboot! started working again

--- script.js
+++ script.js
@@ -10,7 +10,26 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
-function addBookToLibrary() {
-
-
+function addBookToLibrary(book) {
+    return myLibrary.push(book)
 }
+
+function render(myLibrary) {
+    const library = document.getElementById('library');
+    for (let book of myLibrary) {
+        let card = document.createElement('li');
+
+        Object.keys(book).forEach(prop => console.log(`${prop}: ${book[prop]}`));
+
+        card.append('h2');
+        library.append(card);
+    }
+}
+
+const lol = new Book("lol", "sh", "178", true);
+addBookToLibrary(lol);
+
+const hehe = new Book("hehe", "sH", "69", false);
+addBookToLibrary(hehe);
+
+render(myLibrary);
